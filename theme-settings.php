a8ab186b6089cd8df1e29f070e7954203e34deea
palagpatski <palagpatski@gmail.com>
2017-10-06 13:58:22 +0800

    Change name

--- theme-settings.php
+++ theme-settings.php
@@ -15,7 +15,7 @@ function dgr_rubik_form_system_theme_settings_alter(&$form, $form_state, $form_i
 
   $form['rubik'] = array(
     '#type' => 'fieldset',
-    '#title' => t('WWM Rubik'),
+    '#title' => t('DGR Rubik'),
   );
   $form['rubik']['rubik_show_branding'] = array(
     '#type' => 'checkbox',
