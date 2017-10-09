ee9771320fefd5657515b7d276212fb17bde4760
palagpatski <palagpatski@gmail.com>
2017-10-09 14:11:10 +0800

    Move breadcrumbs out of the branding region

--- page.tpl.php
+++ page.tpl.php
@@ -12,13 +12,7 @@
   <?php print render($page['navbar']); ?>
 </header>
 <?php if (theme_get_setting('rubik_show_branding')): ?>
-  <div id='branding'><div class='limiter clearfix'>  
-    <?php if ((arg(0) == 'user' && arg(1) == 'login') || (arg(0) == 'user' && arg(1) == 'password')): ?>
-      <div class='breadcrumb clearfix'></div>
-    <?php else: ?>
-      <div class='breadcrumb clearfix'><?php print $breadcrumb ?></div>
-    <?php endif; ?>
-  
+  <div id='branding'><div class='limiter clearfix'>    
     <?php if (!$overlay && isset($secondary_menu)) : ?>
       <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('class' => 'links secondary-menu'))) ?>
     <?php endif; ?>
@@ -44,6 +38,10 @@
 
 <?php if ($show_messages && $messages): ?>
 <div id='console'><div class='limiter clearfix'><?php print $messages; ?></div></div>
+<?php endif; ?>
+
+<?php if ($breadcrumb): ?>
+  <div class='breadcrumb clearfix'><?php print $breadcrumb ?></div>
 <?php endif; ?>
 
 <div id='page'><div id='main-content' class='limiter clearfix'>
