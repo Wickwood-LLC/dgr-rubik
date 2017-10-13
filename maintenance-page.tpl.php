b40c4e6329b5c0666bdef93ec3c1d8e6c84561c2
palagpatski <palagpatski@gmail.com>
2017-10-13 12:48:57 +0800

    Hide logo if there are none

--- maintenance-page.tpl.php
+++ maintenance-page.tpl.php
@@ -34,7 +34,7 @@
           <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
             <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
           </a>
-        <?php endif: ?>
+        <?php endif; ?>
         <div id="name-and-slogan">
             <div id="site-name">
               <strong>
