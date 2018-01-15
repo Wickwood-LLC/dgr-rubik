d4669643a2f3fcb151df7b89aa2af0508b60ba41
palagpatski <palagpatski@gmail.com>
2018-01-15 13:00:48 +0800

    Responsive equal height

--- dgr_rubik.js
+++ dgr_rubik.js
@@ -137,4 +137,49 @@
       $('body').on("DOMNodeInserted", ".boxed", wrap);  // for dynamically loaded elements (after page load)
     }
   };
+
+  Drupal.behaviors.responsiveEqualHeight = {    
+    attach: function (context, settings) {
+      equalheight = function(container){
+
+        var currentTallest = 0,
+            currentRowStart = 0,
+            rowDivs = new Array(),
+            $el,
+            topPosition = 0;
+        $(container).each(function() {
+
+          $el = $(this);
+          $($el).height('auto')
+          topPostion = $el.position().top;
+
+          if (currentRowStart != topPostion) {
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+              rowDivs[currentDiv].height(currentTallest);
+            }
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPostion;
+            currentTallest = $el.height();
+            rowDivs.push($el);
+          } else {
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+          }
+          for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+          }
+        });
+      }
+
+      $(window).load(function() {
+        equalheight('.pane-articles-test-panel-pane-6 .views-row');
+      });
+
+
+      $(window).resize(function(){
+        equalheight('.pane-articles-test-panel-pane-6 .views-row');
+      });
+    }
+  };
+
 }(jQuery));
