af7352cfe06c93e2b000ec24c652d3d7c77c697f
Junaid PV <junaid@detlus.com>
2018-09-07 15:55:28 +0530

    Make dgr_rubik_style.css for screen and remove manually loading, which was coded to override rubik's style.css

--- template.php
+++ template.php
@@ -318,11 +318,6 @@ function dgr_rubik_preprocess_html(&$vars) {
    ),
   );
   drupal_add_html_head($viewport, 'viewport');
-
-  drupal_add_css(path_to_theme().'/dgr_rubik_style.css', array(
-    'group' => CSS_THEME,
-    'preprocess' => FALSE
-  ));
 }
 
 /**
