bdcc53e51f1672a9fc79ae8fd827447c5e26f5e0
palagpatski <palagpatski@gmail.com>
2018-10-25 16:05:53 +0800

    dpm content

--- templates/print--node--press-release.tpl.php
+++ templates/print--node--press-release.tpl.php
@@ -98,6 +98,7 @@
       <h2 class="print-title"><?php print $print_title; ?></h2>
     <?php endif; ?>
     <h1> TESTING </h1>
+    dpm($content);
     <div class="print-content"><?php print $content; ?></div>
     <div class="print-footer"><?php print theme('print_footer'); ?></div>
     <hr class="print-hr" />
